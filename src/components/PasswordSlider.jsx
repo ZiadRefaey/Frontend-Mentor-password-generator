ca3c87e07a789f6f17797509f3ae8a57d76f5c96
ZiadRefaey <99500161+ZiadRefaey@users.noreply.github.com>
2023-10-15 10:09:15 +0300

    slider range

--- src/components/PasswordSlider.jsx
+++ src/components/PasswordSlider.jsx
@@ -2,14 +2,18 @@ export default function PasswordSlider({ passwordLength, dispatch }) {
   return (
     <div className="w-full">
       <div className="w-full flex items-center justify-between pb-4">
-        <p className="body-sm md:heading-lg text-offWhite font-bold">
+        <label
+          htmlFor="slider"
+          className="body-sm md:heading-lg text-offWhite font-bold"
+        >
           Character Length
-        </p>
+        </label>
         <h2 className="text-custom-md md:text-custom-lg text-neonGreen font-bold">
           {passwordLength}
         </h2>
       </div>
       <input
+        id="slider"
         type="range"
         min="1"
         max="20"
